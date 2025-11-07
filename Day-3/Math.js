c9fcc05dd059f4bb2d42e6d3ed79793ef543cf9d
Monir1231 <monirmolla324@gmail.com>
2025-11-07 23:14:37 +0600

    day-3

---
+++ Day-3/Math.js
@@ -0,0 +1,43 @@
+// Math.round()
+
+let num = Math.round(4.3)
+console.log(num)
+
+// Math.ceil() returns the value of x rounded up to its nearest integer:
+
+let number = Math.ceil(4.2)
+console.log(number)
+
+// Math.floor() returns the value of x rounded down to its nearest integer:
+
+let numfloor = Math.floor(4.3);
+console.log(numfloor)
+
+// Max value 
+
+let maxvalue = Math.max(2,5,67,8)
+console.log(maxvalue)
+
+// low value
+
+let lowvalue = Math.min(2,5,67,8)
+console.log(lowvalue)
+
+// Math.pow(x, y) returns the value of x to the power of y:
+
+let pow = Math.pow(2,3)
+console.log(pow)
+
+// Math.sqrt(x) returns the square root of x:
+
+let sqrt = Math.sqrt(16)
+console.log(sqrt)
+
+// JavaScript Random
+
+let random = Math.random()
+console.log(random)
+
+let floorRandom = Math.floor (Math.random() * 10) + 1  // 1 to 10
+
+console.log(floorRandom)
---
+++ Day-3/NumberMethods.js
@@ -0,0 +1,19 @@
+// toFixed(), parseInt(), parseFloat()
+
+// toFixed() number to strinh
+let x = 123;
+let text = x.toString(2);
+
+console.log(typeof text)
+
+// parseInt() string to number
+let y = "345"
+
+let num = parseInt(y)
+console.log( typeof num)
+
+// parseFloat()
+
+let num1 = 3.677
+let mynum = parseFloat(num1)
+console.log(mynum)
