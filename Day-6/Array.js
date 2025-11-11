4021f8ea28130e2b1ff21289fc39d402e04e5a24
Monir1231 <monirmolla324@gmail.com>
2025-11-11 18:24:07 +0600

    day-6

---
+++ Day-6/Array.js
@@ -0,0 +1,39 @@
+
+// array
+let numArray = [1,2,3,"Monir","nasir"]
+console.log(numArray)
+
+// value change '
+let number = [1,2,3,45]
+ number[0] = 35
+console.log(number)
+
+// JavaScript Array push()
+let name = ["Monir","Rana"]
+ name.push("Ruma")
+console.log(name)
+
+// pop() js
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+ fruits.pop()
+console.log(fruits)
+
+// Getting the array size
+let mountains = ['Everest', 'Fuji', 'Nanga Parbat'];
+console.log(mountains.length)
+
+// shift() method
+
+let pat = ["dog","cat"]
+let remove = pat.shift()
+console.log(remove)
+
+// unshift() method
+let seas = ['Black Sea', 'Caribbean Sea', 'North Sea', 'Baltic Sea'];
+seas.unshift('green Sea');
+console.log(seas)
+
+// avaScript Array join()
+const favfruits = ["Banana", "Orange", "Apple", "Mango"];
+let add = favfruits.join("*")
+console.log(add)
